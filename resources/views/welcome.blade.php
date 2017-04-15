3bbcacd1dd8d9b220428fd0e6987004e5f041462
Naveen Niraula <subtlenv@gmail.com>
2017-04-15 17:53:07 +0545

    regular update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,35 +10,36 @@
 	<nav class="navbar navbar-inverse">
 	  <div class="container-fluid">
 	    <ul class="nav navbar-nav">
-	      <li><a href="#">learn</a></li>
+	      <li><a href="#">what ?</a></li>
 	    </ul>
 	  </div>
 	</nav>
 
 	<div class="container-fluid jumbotron text-center n-logo">
-		<h1 class="n-heading">PSN</h1>
-		<p>the real fun starts here.</p>
+		<div class="inner-wrapper border-top n-cw">
+			<h1 class="n-heading">PSN</h1>
+			<p>the real fun starts here.</p>
+		</div>
 	</div>
 
-	<div class="n-space"></div>
 
 	<div class="container-fluid n-login">
-		<form method="POST" action="/authenticate">
-		  <div class="input-group">
-		    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-		    <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-		  </div>
-		  <div class="input-group">
-		    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-		    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
-		  </div>
-		  <div class="n-actions">
-		  	<input type="submit" class="btn btn-default" value="Login"><a href="#" class="btn btn-default btn-link">Register Now ?</a>
-		  </div>
-		</form>
+		<div class="n-inwr">
+			<form method="POST" action="/authenticate">
+			  <div class="input-group">
+			    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+			    <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+			  </div>
+			  <div class="input-group">
+			    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+			    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+			  </div>
+			  <div class="n-actions">
+			  	<input type="submit" class="btn btn-default" value="Login"><a href="#" class="btn btn-default btn-link">Register Now ?</a>
+			  </div>
+			</form>
+		</div>
 	</div>
-	
-	<div class="n-space"></div>
 
 	<footer>
 		<div class="container-fluid n-container">
