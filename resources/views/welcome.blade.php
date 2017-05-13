8d0b49b5ec83f81dbb15415231561e86c66a79a3
Naveen Niraula <subtlenv@gmail.com>
2017-05-13 18:06:55 +0545

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,30 @@
 <head>
 	<meta charset="utf-8" />
 	<title>PSN :: Homepage</title>
+	<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/base.css') }}">
 </head>
 <body>
+
+	<!-- <div class="header">
+		<div class="container">
+			<div class="jumbotron">
+				<h1>lets start.</h1>
+			</div>
+		</div>
+	</div> -->
+	<div class="logo"></div>
 	<div class="nav">
 		<div class="container-fluid">
 			<nav>
 				<ul>
-					<li><a href="#">Register</a></li>
-					<li><a href="#">Login</a></li>
+					<li><a href="#">login</a></li><!--
+					--><li><a href="#">register</a></li><!--
+					--><li><a href="#">login with facebook</a></li>
 				</ul>
 			</nav>
 		</div>
-
 	</div>
+
 </body>
 </html>
