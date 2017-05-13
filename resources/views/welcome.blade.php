a8c29541522d571838f9d4194aace58b7e2a7de5
Naveen Niraula <subtlenv@gmail.com>
2017-05-13 17:06:49 +0545

    jfgh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,19 @@
 <head>
 	<meta charset="utf-8" />
 	<title>PSN :: Homepage</title>
-	@include('required/css')
+	<link rel="stylesheet" href="{{ asset('/css/base.css') }}">
 </head>
 <body>
+	<div class="nav">
+		<div class="container-fluid">
+			<nav>
+				<ul>
+					<li><a href="#">Register</a></li>
+					<li><a href="#">Login</a></li>
+				</ul>
+			</nav>
+		</div>
 
-	<div class="n-center">
-		<h1>Project Social Network</h1>
 	</div>
-
-	<div class="n-box n-form-container">
-		<form action="/authenticate" method="POST">
-			<span class="n-inline-element"><legend>Login</legend>
-			<input type="text" name="un" required="required" placeholder="Username" />
-			<input type="password" name="pw" required="required" placeholder="Password" />
-			<input type="submit" value="Login" /></span>
-			<span class="n-inline-element">
-				<a href="/register">Register here.</a>
-				&nbsp;&nbsp;&nbsp;OR&nbsp;&nbsp;&nbsp; login with
-				<a href="">Facebook</a>
-			</span>
-		</form>
-	</div>
-
 </body>
 </html>
